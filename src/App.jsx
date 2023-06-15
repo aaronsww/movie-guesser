d1b9209ba63ee57460b7996356115038fc79a3a1
Jeevan Aaron <100500810+aaronsww@users.noreply.github.com>
2023-06-15 06:34:44 +0530

    chore: setup base functionality with api call

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,60 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState, useEffect } from "react";
+import "./App.css";
+import axios from "axios";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [movieData, setMovieData] = useState({});
+
+  const movies = [
+    { title: "Avatar", year: 2009 },
+    { title: "Avengers: Endgame", year: 2019 },
+    { title: "Titanic", year: 1997 },
+    { title: "Star Wars: Episode VII - The Force Awakens", year: 2015 },
+    { title: "Jurassic World", year: 2015 },
+    { title: "The Lion King", year: 2019 },
+    { title: "The Avengers", year: 2012 },
+    { title: "Inception", year: 2010 },
+    { title: "Frozen II", year: 2019 },
+    { title: "Avengers: Infinity War", year: 2018 },
+  ];
+
+  const fetchData = async (randomMovie) => {
+    try {
+      const response = await axios.get(
+        `https://api.themoviedb.org/3/search/movie?api_key=${
+          import.meta.env.VITE_MOVIE_API_KEY
+        }&query=${randomMovie.title}&year=${randomMovie.year}`
+      );
+      setMovieData(response.data.results[0]);
+      console.log(response.data.results[0]);
+      console.log(movieData);
+    } catch (error) {
+      console.error("Error fetching data:", error);
+    }
+  };
+
+  useEffect(() => {
+    const randomIndex = Math.floor(Math.random() * movies.length);
+    const randomMovie = movies[randomIndex];
+    console.log(randomMovie.title);
+    fetchData(randomMovie);
+  }, []);
 
   return (
     <>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        {movieData && (
+          <div key={movieData.id}>
+            <h2>{movieData.title}</h2>
+            <img
+              src={`https://image.tmdb.org/t/p/w500${movieData.backdrop_path}`}
+              alt="Movie Backdrop"
+            />
+          </div>
+        )}
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;

8edec0fe3fc6ae49c252559905df87e40f4ceb36
Jeevan Aaron <100500810+aaronsww@users.noreply.github.com>
2023-06-30 18:32:20 +0530

    feat: added gifs

--- src/App.jsx
+++ src/App.jsx
@@ -3,21 +3,32 @@ import "./App.css";
 import { movies } from "./movieData.jsx";
 import axios from "axios";
 
+import rightGif from "./gifs/right/a.gif";
+import wrongGif from "./gifs/wrong/c.gif";
+
+// const rightGifs = [
+//  "./gifs/right/a.gif",
+//   "./gifs/right/b.gif",
+//   "./gifs/right/c.gif",
+//   "./gifs/right/d.gif",
+// ];
+
+// const wrongGifs = [
+//   "./gifs/wrong/c.gif",
+//  "./gifs/wrong/d.gif",
+
+// ];
+
 function App() {
   const [movieData, setMovieData] = useState({});
   const [guess, setGuess] = useState("");
   const [suggestions, setSuggestions] = useState([]);
-
+  const [correctGuess, setCorrectGuess] = useState(null);
   const [randomMovie, setRandomMovie] = useState(
     movies[Math.floor(Math.random() * movies.length)]
   );
 
   const [indicator, setIndicator] = useState(true);
-  
-  useEffect(() => {
-   if(guess === "")
-    setIndicator(true)
-  }, [guess]);
 
   // const [index, setIndex] = useState(0);
   // const [src, setSrc] = useState(
@@ -31,6 +42,8 @@ function App() {
       setIndex(index + 1);
       // setSrc(`${movies[0].path}${movies[0].stills[index + 1]}.jpg`);
       setSrc(randomMovie.path[index + 1]);
+    } else {
+      setIndicator(0);
     }
   }
 
@@ -69,31 +82,51 @@ function App() {
   useEffect(() => {
     console.log(randomMovie);
     fetchData(randomMovie);
-  }, []);
+  }, [randomMovie]);
 
   function checkGuess() {
     if (guess === randomMovie.title) {
       setRandomMovie(movies[Math.floor(Math.random() * movies.length)]);
       setGuess("");
-    } else alert("Try Again");
+      setIndicator(null);
+      setCorrectGuess(true);
+      console.log(randomMovie);
+    } else {
+      setCorrectGuess(false);
+      setIndicator(true);
+    }
   }
 
   const handleInputChange = (event) => {
     setGuess(event.target.value);
-    setIndicator(false)
+    setIndicator(false);
     const value = event.target.value;
-
-    // Fetch movie title suggestions based on input value
     fetchMovieSuggestions(value);
   };
+
+  // const getRandomGif = (gifs) => {
+  //   const randomIndex = Math.floor(Math.random() * gifs.length);
+  //   return gifs[randomIndex];
+  // };
 
   return (
     <>
       <div className="search-bar">
         <div className="search-btn">
-          <input placeholder="Search for a movie" type="text" value={guess} onChange={handleInputChange} />
-          {indicator && <button onClick={handleClick}>Skip Frame</button>}
-          {!indicator &&<button onClick={checkGuess}>Submit</button>}
+          <input
+            placeholder="Search for a movie"
+            type="text"
+            value={guess}
+            onChange={handleInputChange}
+          />
+          {indicator === true && (
+            <button onClick={handleClick}>Skip Frame</button>
+          )}
+          {indicator === false && <button onClick={checkGuess}>Submit</button>}
+          {indicator === 0 && <button>Out of skips</button>}
+          {indicator === null && (
+            <button onClick={() => window.location.reload()}>Next Movie</button>
+          )}
         </div>
         {guess.length > 0 && suggestions.length > 0 && (
           <ul className="suggestions">
@@ -111,11 +144,20 @@ function App() {
           </ul>
         )}
       </div>
+
+      {correctGuess === true && (
+        <img className="gif" src={rightGif} alt="Correct GIF" />
+      )}
+      {correctGuess === false && (
+        <img className="gif" src={wrongGif} alt="Wrong GIF" />
+      )}
+
       <div>
         {movieData && (
           <div key={movieData.id}>
             {/* <h2>{randomMovie.title}</h2> */}
             <img
+              className="still"
               // src={`https://image.tmdb.org/t/p/w500${movieData.backdrop_path}`}
               src={src}
               alt="Movie Backdrop"
@@ -123,8 +165,6 @@ function App() {
           </div>
         )}
       </div>
-
-   
     </>
   );
 }
